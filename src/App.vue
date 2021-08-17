35a2e2b0cb10f4669cf08295a8fe238cd331b6ea
yash kanakiya <yashkanakiya281297@gmail.com>
2021-08-17 12:37:24 +0530

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,106 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="flex flex-col items-center space-y-10 py-5 bg-gray-800 md:mx-20">
+      <h1 class="text-5xl text-green-300 font-bold uppercase tracking-wider">create blog</h1>
+      <div class="flex flex-col items-center space-y-2">
+        <label class="text-3xl uppercase text-white">Blog title</label>
+        <input type="text" v-model="blog.title" class="border-2 border-black bg-green-400">
+      </div>
+
+      <div class="flex flex-col items-center space-y-2 ">
+        <label class="text-3xl uppercase text-white">Blog userid</label>
+        <input type="text" v-model="blog.user_id" class="border-2 border-black bg-green-400">
+      </div>
+
+      <div class="flex flex-col items-center space-y-2 ">
+        <label class="text-3xl uppercase text-white">Blog Contant</label>
+        <textarea cols="50" rows="10" v-model="blog.body" class="border-2 border-black bg-green-400"></textarea>
+      </div>
+      <button class="border bg-blue-700 text-white px-3 py-1 uppercase tracking-widest font-semibold" @click="add">create</button>
+      <button class="border bg-yellow-300 text-white px-3 py-1 uppercase tracking-widest font-semibold" 
+      @click="up(blog.id)">update</button>
+    </div>
+
+    <!-- preview -->
+    <div class="flex flex-col  py-5 bg-gray-400 space-y-3 md:mx-20" >
+      <p class="text-2xl">BlogTitle: <span class="">{{blog.title}}</span></p>
+      <p class="text-2xl">BlogID: <span class="">{{blog.user_id}}</span></p>
+      <p class="text-2xl">BlogContant:<br><span class="text-base">{{blog.body}}</span></p>
+    </div>
+      <div>
+        <!-- watch all blogs -->
+        <div class="flex flex-col items-center py-5  bg-green-500 space-y-3 p-10 md:mx-20">
+            <h1 class="text-5xl text-green-300 font-bold uppercase tracking-wider ">all blogs articals</h1>
+            <div v-for="post in posts.data" :key="post.title" class="bg-gray-800 text-white p-10 text-center space-y-5">
+                <h1 v-if="post" class="text-3xl">{{post.title}}</h1>
+                <p v-if="post" class="text-xl">{{post.body}}</p>
+                <button class="border bg-red-700 text-white px-3 py-1 uppercase tracking-widest font-semibold"
+                @click="rm(post.id)">delete</button>
+                <button class="border bg-green-700 text-white px-3 py-1 uppercase tracking-widest font-semibold"
+                @click="editPost(post.id)">edit</button>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  
+ data() {
+  return {
+    blog: {
+      title: "",
+      body: "",
+      user_id: "",
+    },
+    upblog: {
+      title: "default",
+      body: "default body",
+      user_id: "54",
+    },
+    posts: [],
   }
+ },
+created() {
+  this.axios.get('v1/posts').then(data => {
+  console.log(data)
+  this.posts = data.data
+  })
+},
+methods: {
+  add() {
+    this.axios.post('v1/posts',this.blog).then(data =>{
+    console.log(data)
+    this.blog = ""
+    })
+    this.submitted = true
+  },
+  rm(id) {
+    this.axios.delete(`v1/posts/${id}`).then(data => {
+    console.log(data)
+    })
+    },
+  editPost(id) {
+    this.axios.get(`v1/posts/${id}`, this.blog).then(data => {
+    console.log(data)
+    let blog = data.data.data
+    this.blog = blog
+    })
+  },
+  up(id) {
+    this.axios.put(`v1/posts/${id}`, this.blog).then(data => {
+    console.log(data)
+    this.blog = ""
+    })
+  }
+},
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@/assets/style/index.css';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+axios.defaults.baseURL = 'https://gorest.co.in/public/'
+axios.defaults.headers.common['Content-Type'] = 'application/json'
+axios.defaults.headers.common = {'Authorization': `Bearer ${'286dab38c44ed569871e8337b06912ec49cac8a7135f2deefbf999e580edda79'}`}
+export default axios;
 
 Vue.config.productionTip = false
 
